9252de918c430156c9e00720e39e988e0069a346
anelia <anelia.aim13@gmail.ru>
2020-11-30 15:40:41 +0400

    vue icon delete

--- toDo/app/js/main.js
+++ toDo/app/js/main.js
@@ -3,16 +3,44 @@ new Vue({
     data: {
         text: null,
         tasks: [
-            'Сделать из Ани классного программиста:)'
+            {
+                taskStatus: false,
+                taskText: 'Сделать из Ани суперпрограммиста'
+            }
+
+
+
         ]
 
     },
     methods: {
         add: function () {
             if (this.text !== '') {
-                this.tasks.push(this.text)
+                this.tasks.push({
+                    taskStatus: false,
+                    taskText: this.text
+                })
                 this.text = ''
             }
+        },
+        changeTaskStatus: function (task) {
+            task.taskStatus = !task.taskStatus
+        },
+        deleteTask: function (index) {
+            this.$delete(this.tasks, index)
+        },
+        statusIcon: function (task) {
+            return {
+                'glyphicon-unchecked': task.taskStatus === false,
+                'glyphicon-check': task.taskStatus === true
+            }
+
+        },
+        statusItem: function (task) {
+            return {
+                'app__tasks-done': task.taskStatus === true
+            }
         }
+
     }
 })
